3108fcee6e3bb91ccffda39ee42552474934228b
drgmk <gkennedy@ast.cam.ac.uk>
2020-03-11 14:51:45 +0000

    update gauss, add rebin2d

--- dd/imaging.py
+++ dd/imaging.py
@@ -24,7 +24,7 @@ def gauss_2d(x_off, y_off, peak, sig_maj, sig_min, pa, size, norm=False):
     '''
     ny, nx = size
     yc, xc = (ny-1)/2, (nx-1)/2
-    x, y = np.meshgrid(np.arange(nx)-xc-x_off, np.arange(ny)-yc-y_off)
+    y, x = np.meshgrid(np.arange(nx)-xc-x_off, np.arange(ny)-yc-y_off)
     xp = x * np.cos(np.deg2rad(pa)) - y * np.sin(np.deg2rad(pa))
     yp = x * np.sin(np.deg2rad(pa)) + y * np.cos(np.deg2rad(pa))
     g = peak * np.exp( -(((xp)/sig_maj)**2+
@@ -33,3 +33,17 @@ def gauss_2d(x_off, y_off, peak, sig_maj, sig_min, pa, size, norm=False):
         g /= np.sum(g)
 
     return g
+
+
+def rebin2d(a, shape):
+    """Rebin a 2d array to a smaller size.
+
+    Parameters
+    ----------
+    a : 2d array
+        Input array.
+    shape : length-2 list or array
+        Shape of output array.
+    """
+    sh = shape[0],a.shape[0]//shape[0],shape[1],a.shape[1]//shape[1]
+    return a.reshape(sh).mean(-1).mean(1)
