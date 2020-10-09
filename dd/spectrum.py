8172910e6e17613b3422dbdd0a4ed8629ae88fcc
Grant Kennedy <g.kennedy@warwick.ac.uk>
2020-10-09 16:05:43 +0100

    add rotation profile

--- dd/spectrum.py
+++ dd/spectrum.py
@@ -37,3 +37,36 @@ def bnu_wav_micron(wav_um,temp):
             return cfg.tiny
         else:
             return fact1/(np.exp(fact2)-1.0)
+
+
+def rotation_profile(wave, center, v, depth, w_true=1):
+    '''Return a stellar rotation line profile.
+
+    See 1933MNRAS..93..478C.
+
+    Parameters
+    ----------
+    wave : ndarray of float
+        Wavelengths at which to compute the spectrum.
+    v : float
+        V sin i of the star in km/s.
+    w_true : float
+        True width of line in km/s (assumed Gaussian).
+    '''
+
+    c = 299792.458 # km/s
+    beta = v/c
+    xi = (wave - center)/center
+    nxi = len(xi)
+    
+   
+    def I(xi):
+        '''true line shape'''
+        return 1-depth*np.exp(-(xi/(2*w_true/c))**2)
+
+    t = np.linspace(-1,1,1000)
+    spec = np.zeros(nxi)
+    for i,x in enumerate(xi):
+        spec[i] = 2/np.pi*np.trapz(I(x+beta*t) * np.sqrt(1-t**2), x=t)
+
+    return spec
